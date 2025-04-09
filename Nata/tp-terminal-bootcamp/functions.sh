bc6f43e4dbf8ce39bddb1f2ea8a04edffd5b7199
Nataa19 <canteronatanael8@gmail.com>
2025-04-09 15:57:26 -0300

    ANDUVO

--- Nata/tp-terminal-bootcamp/functions.sh
+++ Nata/tp-terminal-bootcamp/functions.sh
@@ -27,7 +27,11 @@ info_o () {
     echo "Eligió la opción $1..."
     sleep 2
     echo "En el siguiente menú tendrá sub-opciones para $2"
+<<<<<<< HEAD
     sleep 3
+=======
+    sleep 2
+>>>>>>> d4e63ab (ANDUVO)
     echo "Seleccione una opción"
     echo "$3"
     echo "$4"
@@ -40,7 +44,11 @@ info_o () {
 
 funcion_ejecucion () {
     echo "Ejecutando..."
+<<<<<<< HEAD
     sleep 2
+=======
+    sleep 1
+>>>>>>> d4e63ab (ANDUVO)
     echo -e "\n"
 }
 
@@ -59,6 +67,11 @@ while :
         echo "6. Leer el Log 'linux-functions.txt'"
         echo -e "\n"
         solicitar_info
+<<<<<<< HEAD
+=======
+        echo -e "\n"
+    
+>>>>>>> d4e63ab (ANDUVO)
     case $opcion in
     1) 
         
@@ -67,6 +80,7 @@ while :
         echo -e "\n"
             case $o1 in
             1)funcion_ejecucion
+<<<<<<< HEAD
             (sudo apt update && sudo apt upgrade -y) | tee -a linux-functions.txt
             ;;
             2)funcion_ejecucion
@@ -74,6 +88,34 @@ while :
             ;;
             3)funcion_ejecucion
             (sudo apt clean && sudo apt autoremove) | tee -a linux-functions.txt
+=======
+            sudo apt update && sudo apt upgrade -y
+
+                if sudo apt -qq -s upgrade | grep -q '0 actualizados'; then
+                    echo "Sistema actualizado" | tee -a linux-functions.txt
+                else
+                    echo "Actualizando paquetes..."
+                    (sudo apt update && sudo apt upgrade -y) | tee -a linux-functions.txt
+                fi
+            echo -e "\n"    
+            ;;
+
+            2)funcion_ejecucion
+            sudo apt list --upgradable | tee -a linux-functions.txt
+            echo -e "\n"
+            ;;
+
+            3)funcion_ejecucion
+            sudo apt clean -qq && sudo apt autoremove -qq
+
+                if sudo apt -qq autoremove | grep -q '0 para eliminar'; then
+                    echo "Sistema Limpio" | tee -a linux-functions.txt
+                else
+                    echo "Actualizando paquetes..."
+                    (sudo apt clean && sudo apt autoremove) | tee -a linux-functions.txt
+                fi
+            echo -e "\n"
+>>>>>>> d4e63ab (ANDUVO)
             ;;
             esac
        ;;
@@ -81,6 +123,7 @@ while :
     2) info_o "2" "crear directorios/carpetas y comprimidos para backups" "1. Crear una carpeta y 2 archivos txt dentro del directorio actual" "2. Crear una segunda carpeta en el directorio actual" "3. Crear un Backup de Retos en formato comprimido, moverlo a backups-bootcamp-we y descomprimirlo"
        read -n1 -p "Ingrese: " o2
        echo -e "\n"
+<<<<<<< HEAD
             case $o2 in
                 1)funcion_ejecucion
                     mkdir linux-bootcamp-we && mv linux-functions.txt linux-bootcamp-we/ && touch linux-bootcamp-we/history-linux.txt 
@@ -117,6 +160,87 @@ while :
                 ;;
                 7)funcion_ejecucion
                     (ping google.com | head -n 4) | tee -a linux-bootcamp-we/linux-functions.txt
+=======
+            
+            case $o2 in
+                1)funcion_ejecucion
+                    
+                    if [ -d "linux-bootcamp-we" ]; then
+                        echo "Directorio existente"
+                    else
+                        mkdir linux-bootcamp-we && mv linux-functions.txt linux-bootcamp-we/
+                    fi
+
+                    if [ -f "history-linux.txt" ]; then
+                        echo "Archivo existente"
+                    else
+                        touch linux-bootcamp-we/history-linux.txt
+                    fi
+
+                echo -e "\n"
+                ;;
+
+                2)funcion_ejecucion
+                    
+                    if [ -d "backups-bootcamp-we" ]; then
+                        echo "Directorio existente"
+                    else
+                        mkdir backups-bootcamp-we
+                    fi
+                    
+                ;;
+
+                3)funcion_ejecucion
+                    
+                    if [ -f "backups-bootcamp-we/bkp.tar.gz" ]; then
+                        echo "Comprimido existente"
+                    else
+                        (tar -cvzf backups-bootcamp-we/bkp.tar.gz /home/nata/shellcourse/Nata/Retos && tar -xvzf backups-bootcamp-we/bkp.tar.gz -C backups-bootcamp-we) | tee -a linux-bootcamp-we/linux-functions.txt
+                    fi
+
+            esac
+            echo -e "\n"
+            ;;
+
+    3) info_o "3" "obtener la hora del sistema, usuario, version de sistema operativo, kernel y demás" "1. Fecha y hora actual" "2. Directorio actual de trabajo" "3. Mi usuario e información Host, Kernel y Sistema Operativo" "4. Espacio disponible y en uso en el disco" "5. Mostrar los 3 procesos que mas RAM utilizan" "6. Mostrar el historial de comandos" "7. Mostrar el servidor/es que utiliza Google" 
+       read -n1 -p "Ingrese: " o3
+       echo -e "\n"
+            
+            case $o3 in
+                1)funcion_ejecucion
+                    date "+%d/%m/%Y %T" | tee -a linux-bootcamp-we/linux-functions.txt
+                echo -e "\n"    
+                ;;
+
+                2)funcion_ejecucion
+                    pwd | tee -a linux-bootcamp-we/linux-functions.txt
+                echo -e "\n"    
+                ;;
+
+                3)funcion_ejecucion
+                    (whoami && uname -a | tee -a linux-bootcamp-we/linux-functions.txt) | tee -a linux-bootcamp-we/linux-functions.txt
+                echo -e "\n"    
+                ;;
+
+                4)funcion_ejecucion
+                    df -h | tee -a linux-bootcamp-we/linux-functions.txt
+                echo -e "\n"
+                ;;
+
+                5)funcion_ejecucion
+                    (ps -eo user,pid,%mem,stat --sort=-%mem | head -n 4) | tee -a linux-bootcamp-we/linux-functions.txt
+                echo -e "\n"
+                ;;
+
+                6)funcion_ejecucion
+                    history 20 | tee -a linux-bootcamp-we/history-linux.txt 
+
+                ;;
+
+                7)funcion_ejecucion
+                    (ping google.com | head -n 4) | tee -a linux-bootcamp-we/linux-functions.txt
+                echo -e "\n"
+>>>>>>> d4e63ab (ANDUVO)
                 ;;
             esac
             ;;
@@ -124,22 +248,52 @@ while :
     4) info_o "4" "crear Backups, mostrar información de archivos y listar la Home." "1. Crear un comprimido de todo el directorio actual y guardarlo en 'backups-bootcamp-we'" "2. Descomprimir el archivo en la Home/Documents" "3. Mostrar el contenido de los archivos txt creados" "4. Listar el contenido de el archivo descomprimido en Home"
        read -n1 -p "Ingrese: " o4
        echo -e "\n"
+<<<<<<< HEAD
             case $o4 in
                 1)funcion_ejecucion
                     tar -cvzf backups-bootcamp-we/tp-terminal-bootcamp.tar.gz . | tee -a linux-bootcamp-we/linux-functions.txt
                 ;;
+=======
+            
+            case $o4 in
+                1)funcion_ejecucion
+
+                    if [ -f "backups-bootcamp-we/tp-terminal-bootcamp.tar.gz" ]; then
+                        echo "Comprimido existente"
+                    else
+                        tar -cvzf backups-bootcamp-we/tp-terminal-bootcamp.tar.gz . | tee -a linux-bootcamp-we/linux-functions.txt
+                    fi
+
+                echo -e "\n"
+                ;;
+
+>>>>>>> d4e63ab (ANDUVO)
                 2)funcion_ejecucion
                     if [ -d "$HOME/Documents" ]; then
                         tar -xvzf backups-bootcamp-we/tp-terminal-bootcamp.tar.gz -C $HOME/Documents | tee -a linux-bootcamp-we/linux-functions.txt
                     else
                         tar -xvzf backups-bootcamp-we/tp-terminal-bootcamp.tar.gz -C $HOME/Documentos | tee -a linux-bootcamp-we/linux-functions.txt 
                     fi   
+<<<<<<< HEAD
                 ;;
                 3)funcion_ejecucion
                     (cat linux-bootcamp-we/linux-functions.txt && cat linux-bootcamp-we/history-linux.txt) | tee -a linux-bootcamp-we/linux-functions.txt
                 ;;
                 4)funcion_ejecucion
                     ls -l $HOME/tp-terminal-bootcamp | tee -a linux-bootcamp-we/linux-functions.txt
+=======
+                echo -e "\n"
+                ;;
+
+                3)funcion_ejecucion
+                    (cat linux-bootcamp-we/linux-functions.txt && cat linux-bootcamp-we/history-linux.txt) | tee -a linux-bootcamp-we/linux-functions.txt
+                echo -e "\n"
+                ;;
+                
+                4)funcion_ejecucion
+                    ls -l $HOME/tp-terminal-bootcamp | tee -a linux-bootcamp-we/linux-functions.txt
+                echo -e "\n"
+>>>>>>> d4e63ab (ANDUVO)
                 ;;
             esac
             ;;
